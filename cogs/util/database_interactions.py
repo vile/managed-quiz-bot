156860aa5f6ff5ac04f573fffb53396fabd57c14
Vile <111662603+vile@users.noreply.github.com>
2025-01-11 22:00:26 -0500

    feat(bot): create quiz_types and quiz_settings table, add quiz and quiz setting database interactions
    - update some schemas and commands to accommodate new tables

--- cogs/util/database_interactions.py
+++ cogs/util/database_interactions.py
@@ -29,11 +29,22 @@ def create_tables_if_not_exist() -> None:
 
         cursor.execute(
             """
+            CREATE TABLE IF NOT EXISTS quiz_types (
+                id INTEGER PRIMARY KEY,
+                class_type TEXT UNIQUE NOT NULL
+            );
+            """
+        )
+
+        cursor.execute(
+            """
             CREATE TABLE IF NOT EXISTS quiz_question_bank (
                 id INTEGER PRIMARY KEY,
                 question_text TEXT NOT NULL,
+                quiz_type INTEGER NOT NULL,
                 created_by INTEGER UNIQUE NOT NULL,
-                created_at INTEGER NOT NULL DEFAULT (unixepoch('now'))
+                created_at INTEGER NOT NULL DEFAULT (unixepoch('now')),
+                FOREIGN KEY(quiz_type) REFERENCES quiz_types(id)
             );
             """
         )
@@ -50,16 +61,28 @@ def create_tables_if_not_exist() -> None:
             """
         )
 
+        cursor.execute(
+            """
+            CREATE TABLE IF NOT EXISTS quiz_settings (
+                id INTEGER PRIMARY KEY,
+                quiz_type INTEGER UNIQUE NOT NULL,
+                length INTEGER NOT NULL,
+                min_correct INTEGER NOT NULL,
+                FOREIGN KEY (quiz_type) REFERENCES quiz_types(id)
+            );
+            """
+        )
+
 
 def check_if_manager_exists(user_id: int) -> bool:
-    """Query database to check if `user_id` is present in the `managers` database."""
+    """Query database to check if `user_id` is present in the `managers` table."""
     with get_db_context() as (_, cursor):
         cursor.execute(
             """
-        SELECT COUNT(managers.discord_id)
-        FROM managers
-        WHERE managers.discord_id = ?
-        """,
+            SELECT COUNT(managers.discord_id)
+            FROM managers
+            WHERE managers.discord_id = ?;
+            """,
             (user_id,),
         )
 
@@ -67,40 +90,182 @@ def check_if_manager_exists(user_id: int) -> bool:
 
 
 def add_new_manager(manager_id: int, caller_id: int) -> None:
-    """Insert a new manager into the `managers` database using their Discord ID."""
+    """Insert a new manager into the `managers` table using their Discord ID."""
     with get_db_context() as (_, cursor):
         cursor.execute(
             """
-        INSERT INTO managers (discord_id, added_by)
-        VALUES (?, ?);
-        """,
+            INSERT INTO managers (discord_id, added_by)
+            VALUES (?, ?);
+            """,
             (manager_id, caller_id),
         )
 
 
 def remove_current_manager(manager_id: int) -> bool:
-    """Delete an existing manager from the `managers` database using their Discord ID."""
+    """Delete an existing manager from the `managers` table using their Discord ID."""
     with get_db_context() as (_, cursor):
         cursor.execute(
             """
-        DELETE FROM managers
-        WHERE managers.discord_id = ?
-        RETURNING *;
-        """,
+            DELETE FROM managers
+            WHERE managers.discord_id = ?
+            RETURNING *;
+            """,
             (manager_id,),
         )
 
         return len(cursor.fetchall()) >= 1
 
 
-def select_all_managers() -> list[int]:
-    """Select all existing bot managers."""
+def select_all_managers() -> list[tuple[int, int, int, int]]:
+    """Select all existing bot managers from the `managers` table."""
     with get_db_context() as (_, cursor):
         cursor.execute(
             """
-        SELECT *
-        FROM managers;
-        """
+            SELECT *
+            FROM managers;
+            """
         )
 
         return cursor.fetchall()
+
+
+def select_all_quiz_types() -> list[tuple[int, str]]:
+    """Select all existing quiz types from the `quiz_types` table."""
+    with get_db_context() as (_, cursor):
+        cursor.execute(
+            """
+            SELECT *
+            FROM quiz_types;
+            """
+        )
+
+        return cursor.fetchall()
+
+
+def check_if_quiz_type_exists(quiz_type: str) -> bool:
+    """Query database to check if `quiz_type` is present in the `quiz_types` table."""
+    with get_db_context() as (_, cursor):
+        cursor.execute(
+            """
+            SELECT COUNT(quiz_types.class_type)
+            FROM quiz_types
+            WHERE quiz_types.class_type = ?;
+            """,
+            (quiz_type,),
+        )
+
+        return cursor.fetchone()[0] >= 1
+
+
+def select_quiz_str_to_quiz_id(quiz_type: str) -> int:
+    """Convert a quiz str to quiz id."""
+    with get_db_context() as (_, cursor):
+        cursor.execute(
+            """
+            SELECT id
+            FROM quiz_types
+            WHERE quiz_types.class_type = ?;
+            """,
+            (quiz_type,),
+        )
+
+        return cursor.fetchone()[0]
+
+
+def add_quiz_type(quiz_type: str) -> int:
+    """Insert a new quiz type to the `quiz_types` table using a `type` string."""
+    with get_db_context() as (_, cursor):
+        cursor.execute(
+            """
+            INSERT INTO quiz_types (class_type)
+            VALUES (?)
+            RETURNING id;
+            """,
+            (quiz_type,),
+        )
+
+        return cursor.fetchone()[0]
+
+
+def remove_quiz_type(quiz_type: str) -> bool:
+    """Delete an existing quiz type from the `quiz_types` table using a `type` string."""
+    with get_db_context() as (_, cursor):
+        cursor.execute(
+            """
+            DELETE FROM quiz_types
+            WHERE quiz_types.class_type = ?
+            RETURNING *;
+            """,
+            (quiz_type,),
+        )
+
+        return len(cursor.fetchall()) >= 1
+
+
+def select_quiz_settings(quiz_type: str) -> tuple[int, int, int]:
+    """"""
+    with get_db_context() as (_, cursor):
+        cursor.execute(
+            """
+            SELECT qs.id, qs.length, qs.min_correct
+            FROM quiz_settings AS qs
+            LEFT JOIN quiz_types AS qt ON qs.quiz_type = qt.id
+            WHERE qt.class_type = ?;
+            """,
+            (quiz_type,),
+        )
+
+        return cursor.fetchone()
+
+
+def add_quiz_settings(quiz_id: int, quiz_length: int, quiz_min_correct: int) -> None:
+    """"""
+    with get_db_context() as (_, cursor):
+        cursor.execute(
+            """
+            INSERT INTO quiz_settings (quiz_type, length, min_correct)
+            VALUES (?, ?, ?);
+            """,
+            (quiz_id, quiz_length, quiz_min_correct),
+        )
+
+
+def remove_quiz_settings(quiz_id: int) -> bool:
+    """"""
+    with get_db_context() as (_, cursor):
+        cursor.execute(
+            """
+            DELETE FROM quiz_settings
+            WHERE quiz_settings.id = ?
+            RETURNING *;
+            """,
+            (quiz_id,),
+        )
+
+        return len(cursor.fetchall()) >= 1
+
+
+def edit_quiz_settings_length(quiz_length: int, quiz_id: int) -> None:
+    """"""
+    with get_db_context() as (_, cursor):
+        cursor.execute(
+            """
+            UPDATE quiz_settings
+            SET length = ?
+            WHERE quiz_type = ?;
+            """,
+            (quiz_length, quiz_id),
+        )
+
+
+def edit_quiz_settings_min_correct(quiz_min_correct: int, quiz_id: int) -> None:
+    """"""
+    with get_db_context() as (_, cursor):
+        cursor.execute(
+            """
+            UPDATE quiz_settings
+            SET min_correct = ?
+            WHERE quiz_type = ?;
+            """,
+            (quiz_min_correct, quiz_id),
+        )
